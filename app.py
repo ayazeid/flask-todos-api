5f4fc68cdf57371e352e45c0508e5041a8291e9f
ayazeid <aya.zeid95@gmail.com>
2022-02-16 16:59:01 +0200

    Connect with  Database

--- app.py
+++ app.py
@@ -1,57 +1,82 @@
 # imports
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
-# Create Flask class instance
+# # Create Flask class instance
+# def create_app():
 app = Flask(__name__)
+DATABASE_URI = "sqlite:///todos.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# todo list inital data
-todos=[{'title':'Todo Note Title','content':'Todo Note Content'}]
+db = SQLAlchemy(app)
 
-# app routing views
+# todo class model
+class Todo(db.Model):
+    __tablename__ ="Todos"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"Todo: {self.title}"
+
+
+
+
 @app.route('/',methods=['GET','POST'])
 def todoMain():
     """
     The todos main view for listing all todos with GET method, or add new todo with POST method
     """
+    todos = Todo.query.all()
     if request.method == 'GET':
+        todoslist = []
+        for todo in todos:
+            dict={
+                'id':todo.id,
+                'title': todo.title,
+                'content': todo.content
+            }
+            todoslist.append(dict)
+
         return jsonify({
-            'todos': todos
+            'todos': todoslist
         })
+
     elif request.method == 'POST':
         notetitle = request.json.get('title')
         notecontent = request.json.get('content')
-
-        newnote = {
-            'title': notetitle,
-            'content': notecontent
-        }
-
-        todos.append(newnote)
+        newnote = Todo(title=notetitle, content=notecontent)
+        db.session.add(newnote)
+        db.session.commit()
         return 'New Todo Note Has Been Added Successfully.'
 
 
-    
-    
+
 @app.route('/todos/update/<int:id>',methods=['GET','PUT'])
 def todoUpdate(id):
     """
     Get ceriten todo with it's id with GET method, and update that todo with PUT method
     """
-    id-=1
+    todonote = Todo.query.filter_by(id=id).first()
     if request.method == 'GET':
+        todo={
+                'id': todonote.id,
+                'title': todonote.title,
+                'content': todonote.content
+            }
+
         return jsonify({
-            'todos': todos[id]
+            'todos': todo
         })
     elif request.method == 'PUT':
-        notetitle = request.json.get('title')
-        notecontent = request.json.get('content')
-
-        updatenote = {
-            'title': notetitle,
-            'content': notecontent
-        }
-        todos[id]= updatenote
+        todonote.title = request.json.get('title')
+        todonote.content = request.json.get('content')
+        db.session.commit()
         return 'New Todo Note Has Been Updated Successfully.'
+
 
 
 @app.route('/todos/delete/<int:id>',methods=['GET','DELETE'])
@@ -59,16 +84,22 @@ def todoDelete(id):
     """
     Get ceriten todo with it's id with GET method, and remove that todo with DELETE method
     """
-    id-=1
+    todonote = Todo.query.filter_by(id=id).first()
     if request.method == 'GET':
+        todo={
+                'id': todonote.id,
+                'title': todonote.title,
+                'content': todonote.content
+            }
+
         return jsonify({
-            'todos': todos[id]
+            'todos': todo
         })
     elif request.method == 'DELETE':
-        todos.pop(id)
+        db.session.delete(todonote)
+        db.session.commit()
         return 'Todo Note Has Been Deleted Successfully.'
 
 
-
-
+db.create_all()
 app.run(debug=True)
